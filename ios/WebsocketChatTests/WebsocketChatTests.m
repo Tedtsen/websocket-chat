9a94dc4757844f902da63fc125012c6c0de41d2b
Tedtsen <ttw06090@gmail.com>
2021-07-20 01:24:31 +0800

    Project name change

--- ios/WebsocketChatTests/WebsocketChatTests.m
+++ ios/WebsocketChatTests/WebsocketChatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WebsocketChatTests : XCTestCase
+@interface WebSocketChatTests : XCTestCase
 
 @end
 
-@implementation WebsocketChatTests
+@implementation WebSocketChatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
